8f1b51dcd676329e09593a45c1407826f1eb555e
claywatkins8 <claywatkins8@gmail.com>
2021-03-02 15:57:58 -0800

    tweaks

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar.js";
+import { Nav, Col, Container, Row, Button } from "react-bootstrap";
 
 const Projects = () => {
   return (
     <div>
-      <Navbar />
-      <h1>ClaytonWatkins.com</h1>
-      <h5>Projects Page</h5>
+      <Container>
+        <Navbar />
+        <h1>ClaytonWatkins.com</h1>
+        <h5>Projects Page</h5>
+      </Container>
     </div>
   );
 };
