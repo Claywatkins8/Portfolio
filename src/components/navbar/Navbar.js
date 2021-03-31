e738644faea31f8bdf122eb23f549589d58ebfea
claywatkins8 <claywatkins8@gmail.com>
2021-03-31 14:46:44 -0700

    update nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,9 +6,9 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <Container className="navbar-body">
-      <Nav className="justify-content-center">
+      <Nav className="">
         <Nav.Item>
-          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/">Clayton Watkins</Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link href="/about">About</Nav.Link>
